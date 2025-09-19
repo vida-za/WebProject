0c6cb3825cdbc4874ca75a70cfc1dc2f32792a41
vida-za <kuznetsov04.04@gmail.com>
2025-09-19 07:29:26 +0300

    fix env

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const { Client } = require('pg');
 
 const client = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PORT,
   ssl: {
     rejectUnauthorized: false
   }
